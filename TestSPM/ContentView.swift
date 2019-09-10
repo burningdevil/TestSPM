c719c45f005b6b958280a95a8a1f04e3d15c5fa7
kawang <kawang@microstrategy.com>
2019-09-10 18:53:08 +0800

    Initial Commit

---
+++ TestSPM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSPM
+//
+//  Created by kai on 9/10/19.
+//  Copyright © 2019 kai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
