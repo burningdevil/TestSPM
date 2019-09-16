cf2361c1bf7086f42fab15f0e67295f79ef58599
kawang <kawang@microstrategy.com>
2019-09-16 17:17:09 +0800

    add dependency

--- TestSPM/AppDelegate.swift
+++ TestSPM/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let ob = Observable<Any>.of()
         // Override point for customization after application launch.
         return true
     }
